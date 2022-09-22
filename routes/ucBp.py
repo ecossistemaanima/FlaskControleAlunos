f8e0a2557b6d489b03cb260ea2000350b677fa3b
Henrique Poyatos <henrique.poyatos@saojudas.br>
2022-09-21 22:13:02 -0300

    Fizemos o C (Create), U (Update) e D (Delete) completos!

--- routes/ucBp.py
+++ routes/ucBp.py
@@ -1,7 +1,8 @@
 # adiciona o , render_template
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from ..extensions import db
 from ..models.uc import Uc
+from datetime import date, datetime
 
 #Instanciar o blueprint
 ucBp = Blueprint('ucBp', __name__)
@@ -14,3 +15,65 @@ def uc_list():
 #   Adiciona o acesso a banco e a chamada ao render_template
     ucs_query = Uc.query.all()
     return render_template('uc_list.html', ucs=ucs_query)
+
+@ucBp.route('/uc/create')
+def create_uc():
+    return render_template('uc_create.html')
+
+@ucBp.route('/uc/add', methods=["POST"])
+def add_uc():
+
+    sNome = request.form["nome"]
+    sTipo = request.form["tipo"]
+    dInicio = datetime.strptime(request.form["inicio"], '%Y-%m-%d')
+    dFim = datetime.strptime(request.form["fim"], '%Y-%m-%d')
+
+    uc = Uc(nome=sNome, tipo=sTipo, inicio=dInicio, fim=dFim)
+    db.session.add(uc)
+    db.session.commit()
+
+    return redirect(url_for("ucBp.uc_list"))
+
+#Chamar o formulário de alteração
+@ucBp.route('/uc/update/<uc_id>')
+def update_uc(uc_id=0):
+    uc_query = Uc.query.filter_by(id = uc_id).first()
+    return render_template('uc_update.html', uc=uc_query) 
+
+#Tratar o update (faz ele no banco)
+@ucBp.route('/uc/upd', methods=["POST"])
+def upd_uc():
+
+    iUc = request.form["id"]
+    sNome = request.form["nome"]
+    sTipo = request.form["tipo"]
+    dInicio = datetime.strptime(request.form["inicio"], '%Y-%m-%d')
+    dFim = datetime.strptime(request.form["fim"], '%Y-%m-%d')
+
+    uc = Uc.query.filter_by(id = iUc).first()
+    uc.nome = sNome
+    uc.tipo = sTipo
+    uc.inicio = dInicio
+    uc.fim = dFim
+    db.session.add(uc)
+    db.session.commit()
+
+    return redirect(url_for("ucBp.uc_list"))  
+
+#Tratar o apagar (tela de confirmação)
+@ucBp.route('/uc/delete/<uc_id>')
+def delete_uc(uc_id=0):
+    uc_query = Uc.query.filter_by(id = uc_id).first()
+    return render_template('uc_delete.html', uc=uc_query) 
+
+#Para apagar de fato
+@ucBp.route('/uc/dlt', methods=["POST"])
+def dlt_uc():
+
+    iUc = request.form["id"]
+    uc = Uc.query.filter_by(id = iUc).first()
+    db.session.delete(uc)
+    db.session.commit()
+
+    return redirect(url_for("ucBp.uc_list"))
+  
